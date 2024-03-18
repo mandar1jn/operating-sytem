34f250f8e422cd0151ba9c445ff1cd899b7f363b
Mandar1jn <marijnkneppers@gmail.com>
2024-03-18 21:54:24 +0100

    print "Hello World!"

--- boot.asm
+++ boot.asm
@@ -2,12 +2,27 @@ ORG 0x7C00 ; origin
 BITS 16 ; architecture
 
 start:
-	mov ah, 0eh
-	mov al, 'A'
-	mov bx, 0
-	int 0x10
-
+	mov si, message
+	call print
 	jmp $
+
+print:
+	mov bx, 0
+.loop:
+	lodsb
+	cmp al, 0
+	je .done
+	call print_char
+	jmp .loop
+.done:
+	ret
+
+print_char:
+	mov ah, 0eh
+	int 0x10
+	ret
+
+message: db 'Hello World!', 0
 
 times 510-($ - $$) db 0 ; pad to 510 bytes
 dw 0xAA55 ; set magic bytes
