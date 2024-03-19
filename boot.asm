6d642ec55b7792dac17ff08ef800f1e8e1cb1330
Mandar1jn <marijnkneppers@gmail.com>
2024-03-19 19:46:57 +0100

    read messae from disk

--- boot.asm
+++ boot.asm
@@ -10,13 +10,6 @@ times 33 db 0
 entrypoint:
 	jmp 0x7C0:start ; set code segment to 0x7C0
 
-handle_zero:
-	mov ah, 0eh
-	mov al, 'A'
-	mov bx, 0x00
-	int 0x10
-	iret
-
 start:
 	cli ; CLear Interrupts
 	mov ax, 0x7C0
@@ -27,12 +20,25 @@ start:
 	mov sp, 0x7C00 ; set Stack Pointer to 0x7C00
 	sti ; Enables Interrupts
 
-	mov word[ss:0x00], handle_zero
-	mov word[ss:0x02], 0x7C0
+	mov ah, 2 ; read sector command
+	mov al, 1 ; read one sector
+	mov ch, 0 ; cylinder number to 0
+	mov cl, 2 ; read sector 2
+	mov dh, 0 ; head number to 0
+	mov bx, buffer
+	int 0x13
+	
+	jc error
 
-	mov si, message
+	mov si, buffer
 	call print
+
 	jmp $
+
+error:
+	mov si, error_message
+	call print
+	ret
 
 print:
 	mov bx, 0
@@ -50,7 +56,9 @@ print_char:
 	int 0x10
 	ret
 
-message: db 'Hello World!', 0
+error_message: db 'Failed to load sector'
 
 times 510-($ - $$) db 0 ; pad to 510 bytes
 dw 0xAA55 ; set magic bytes
+
+buffer:
