e88bff1a237b5a3d5a1786b1bfca20927fae6550
Mandar1jn <marijnkneppers@gmail.com>
2024-03-20 20:54:58 +0100

    write text to screen

--- src/kernel.c
+++ src/kernel.c
@@ -1,6 +1,13 @@
 #include "kernel.h"
+#include <stdint.h>
 
 void kernel_main()
 {
+	uint16_t* video_mem = (uint16_t*)0xB8000;
 
+
+	for(uint16_t i = 0; i < 26; i++)
+	{
+		video_mem[i] = i + 'A' + (uint16_t)(0xF << 8);
+	}
 }
